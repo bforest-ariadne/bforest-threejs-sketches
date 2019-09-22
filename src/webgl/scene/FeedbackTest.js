d33ab19175b0502554e926b7c3510ec9bf1b528b
Ben Forest <ben@ben-forest.com>
2019-09-22 00:13:26 -0400

    fix feedback scene material

--- src/webgl/scene/FeedbackTest.js
+++ src/webgl/scene/FeedbackTest.js
@@ -2,64 +2,26 @@ const SketchScene = require('./SketchScene');
 const { webgl, assets } = require('../../context');
 // const basicFeedback = require('../postProcessing/basicFeedback');
 // const basicDatamosh = require('../postProcessing/basicDatamosh');
+const { createIronMaterial, ironAssets } = require('../materials/dammagedIron')
 const postProcessSetup = require('../postProcessing/basicDatamosh');
 const query = require('../../util/query');
 const defined = require('defined');
 
 const name = 'feedbacktest';
 
-const path = 'https://vanruesc.github.io/postprocessing/public/demo/textures/skies/space3/';
-const format = '.jpg';
-const names = [ 'px', 'nx', 'py', 'ny', 'pz', 'nz' ];
-let urls = [];
+if ( defined( query.scene ) && query.scene.toLowerCase() === name ) {
+  assets.queue({
+    url: 'assets/textures/studio_small_02_1024/',
+    key: 'env',
+    envMap: true,
+    hdr: true,
+    pbr: true
+  });
 
-for ( let i in names ) {
-  urls[i] = names[i] + format;
+  for ( let i in ironAssets ) {
+    assets.queue( ironAssets[i] );
+  }
 }
-
-var loader = new THREE.CubeTextureLoader();
-loader.setPath( path );
-
-var textureCube = loader.load( urls );
-
-assets.queue({
-  url: 'assets/textures/studio_small_02_1024/',
-  key: 'env',
-  envMap: true,
-  hdr: true,
-  pbr: true
-});
-
-assets.queue({
-  url: 'assets/textures/iron1_bpr/Metal_DamagedIron_2k_metallic.jpg',
-  key: 'iron_m',
-  texture: true
-});
-assets.queue({
-  url: 'assets/textures/iron1_bpr/Metal_DamagedIron_2k_n.jpg',
-  key: 'iron_n',
-  texture: true
-});
-assets.queue({
-  url: 'assets/textures/iron1_bpr/Metal_DamagedIron_2k_roughness.jpg',
-  key: 'iron_r',
-  texture: true
-});
-assets.queue({
-  url: 'assets/textures/iron1_bpr/Metal_DamagedIron_2k_ao.jpg',
-  key: 'iron_a',
-  texture: true
-});
-assets.queue({
-  url: 'assets/textures/iron1_bpr/Metal_DamagedIron_2k_basecolor.jpg',
-  key: 'iron_c',
-  texture: true
-});
-assets.queue({
-  url: 'assets/textures/iron1_bpr/Metal_DamagedIron_2k_h.jpg',
-  key: 'iron_h',
-  texture: true
-});
 
 module.exports = class FeedbackTest extends SketchScene {
   constructor () {
@@ -87,26 +49,12 @@ module.exports = class FeedbackTest extends SketchScene {
     const geometry = new THREE.SphereBufferGeometry(1, 4, 4);
 
     let material, mesh;
-    
-    for (let i = 0; i < 100; ++i) {
-      material = new THREE.MeshStandardMaterial({
-        // color: 0xffffff * Math.random(),
-        color: 0xffffff,
-        roughness: 1.0,
-        metalness: 1.0,
-        // normalMap: crackedNormal,
-        roughnessMap: assets.get('iron_r'),
-        metalnessMap: assets.get('iron_m'),
-        normalMap: assets.get('iron_n'),
-        aoMap: assets.get('iron_a'),
-        map: assets.get('iron_c'),
-        displacement: assets.get('iron_h'),
-        normalScale: new THREE.Vector2(0.1, 0.1),
-        envMap: env.target.texture,
-        flatShading: true
-      });
-      material.needsUpdate = true
 
+    material = createIronMaterial();
+    material.envMap = env.target.texture;
+    material.needsUpdate = true;
+
+    for (let i = 0; i < 100; ++i) {
       mesh = new THREE.Mesh(geometry, material);
       mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
       mesh.position.multiplyScalar(Math.random() * 10);
